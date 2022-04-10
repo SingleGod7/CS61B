1248c79152cde8a45918d9fa0e2239b15acc80f1
unknown <1585305432@qq.com>
2022-04-10 17:17:25 +0800

    remove unnesessary file

--- proj1a/A.java
+++
@@ -1,9 +0,0 @@
-public class A {
-    public static void main(String[] args){
-        LinkedListDeque<Integer> student = new  LinkedListDeque<>();
-        student.removeFirst();
-        student.removeFirst();
-        System.out.print(student.size());
-        System.out.println(student.getRecursive(3));
-    }
-}
