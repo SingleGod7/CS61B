4eeda15608ab5aab8bb1da9f8bccb849f4c2851a
unknown <1585305432@qq.com>
2022-06-07 21:13:22 +0800

    finish homework2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,90 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF percolationGraph;
+    private WeightedQuickUnionUF fullGraph;
+    private boolean[][] openGraph;
+    private int N;
+    private int openCnt;
 
+    public Percolation(int N){
+        percolationGraph = new WeightedQuickUnionUF(N * N);
+        fullGraph = new WeightedQuickUnionUF(N * N);
+        this.N = N;
+        this.openGraph = new boolean[N][N];
+
+        //init the openGraph
+        for (int i = 0 ; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                openGraph[i][j] = false;
+            }
+        }
+
+        //union the bottom and top
+        for (int i = 0; i < N; i++) {
+            percolationGraph.union(0, i);
+            fullGraph.union(0, i);
+            fullGraph.union((N - 1) * N, (N - 1) * N + i);
+        }
+    }
+
+    public void open(int row, int col) {
+        checkValidCoordinate(row, col);
+        openGraph[row][col] = true;
+        openCnt += 1;
+        int[] direction = {-1, 1};
+        int index = row * N + col;
+        for (int i : direction) {
+            if (row + i >= 0 && row + i < N && openGraph[row + i][col]) {
+                percolationGraph.union(index, index + N * i);
+                fullGraph.union(index, index + N * i);
+            }
+            if (col + i >= 0 && col + i < N && openGraph[row][col + i]) {
+                percolationGraph.union(index, index + i);
+                fullGraph.union(index, index + i);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        checkValidCoordinate(row, col);
+        return openGraph[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row == 0) {
+            return openGraph[row][col];
+        } else if (row == N - 1){
+            return fullGraph.connected(0, row * N + col) && openGraph[row][col] && percolationGraph.connected(0, row * N + col);
+        } else {
+            return fullGraph.connected(0, row * N + col) && percolationGraph.connected(0, row * N + col);
+        }
+    }
+
+    public int numberOfOpenSites() {
+        return this.openCnt;
+    }
+
+    public boolean percolates() {
+        return fullGraph.connected(0, N * (N - 1));
+    }
+
+    /** helper function */
+    private void checkValidCoordinate(int row, int col) {
+        checkValidIndex(row);
+        checkValidIndex(col);
+    }
+
+    private void checkValidIndex(int index) {
+        if (index < 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        if (index > (N - 1)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,60 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdDraw;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    private int T;
+    private double[] simulationResult;
+    private double mean;
+    private double stddev;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        this.T = T;
+        this.simulationResult = new double[T];
+
+        for(int i = 0; i < T; i++) {
+            Percolation simulation = pf.make(N);
+            while (!simulation.percolates()) {
+                int col = StdRandom.uniform(N);
+                int row = StdRandom.uniform(N);
+                if (simulation.isOpen(row, col)) {
+                    continue;
+                }
+                simulation.open(row, col);
+
+            }
+            PercolationVisualizer.draw(simulation, N);
+            StdDraw.pause(1000);
+            simulationResult[i] = (double) simulation.numberOfOpenSites() / (double) (N * N);
+        }
+    }
+
+    public double mean() {
+        this.mean = StdStats.mean(simulationResult);
+        return this.mean;
+    }
+
+    public double stddev() {
+        this.stddev = StdStats.stddev(simulationResult);
+        return this.stddev;
+    }
+
+    public double confidenceLow() {
+        return this.mean - 1.96 * this.stddev / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return this.mean + 1.96 * this.stddev / Math.sqrt(T);
+    }
+
+    public static void main(String[] args) {
+        PercolationFactory b = new PercolationFactory();
+        PercolationStats a = new PercolationStats(10, 30, b);
+    }
 }
